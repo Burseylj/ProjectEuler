b79d72d5d9ed6310c2b052c4047de861673735b7
burseylj <burseylj@users.noreply.github.com>
2017-05-06 02:02:23 -0400

    Finished P2, figured out a nice property of fib numbers to reduce runtime

--- P2.py
+++ P2.py
@@ -13,14 +13,47 @@
 # Not sure how to use this information... Could use the formula found at:
 # https://en.wikipedia.org/wiki/Fibonacci_number#Computation_by_rounding
 # However, I'm not convinced that computing n/3 powers of phi will be much more efficient
-# than just doing n summations... also the FP error will be unsightly
+# than just doing n additions... also the FP error will be unsightly
 # we'll see, I guess
 
 # math.floor((phi**12)/2 + 0.5)
 
-# Yep, this is disasterously divergent. Fuck it, let's just brute force this thing
+# Yep, this is disasterously divergent. Let's just brute force this thing
+import timeit
 
-a = 1
-b = 2
+def naive_solution():
+    a = 1
+    b = 2
+    c = 0
+    total = b 
+    while (a+b) <= 4000000: # 4 million
+        c = a
+        a = b
+        b = c + b
+        if (b%2 ==0):
+            total = total +b
+    return total
+ 
+# Actually, the even terms are related by the recursive relation:
+# E(n) = 4*E(n-1) + E(n-2)
+# pf. in pdf
 
-    
+def clever_solution():
+    a = 2
+    b = 8
+    c = 0
+    total = a + b 
+    while (a+4*b) <= 4000000: # 4 million
+        c = a
+        a = b
+        b = 4*b + c
+        if (b%2 ==0):
+            total = total +b
+    return total
+
+print "Naive solution", naive_solution()
+print "timeit results:", timeit.timeit(naive_solution) 
+print "Clever solution", clever_solution()
+print "timeit results:", timeit.timeit(clever_solution)
+
+# So this solution is faster for sure. Cool.
